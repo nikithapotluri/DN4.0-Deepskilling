06d3816351f068dc3e1bd9d675e3b6191103bafe
Nikitha <mailnikitha1710@gmail.com>
2025-06-20 15:09:49 +0530

    Add Outputs

--- WEEK-1/Design Patterns and Principles/Exercise 2_FactoryMethodPatternExample/DocumentFactory.java
+++
@@ -1,7 +0,0 @@
-public abstract class DocumentFactory {
-    public abstract Document createDocument();
-}
-
-abstract class Document {
-    // Define common methods and properties for Document here
-}
