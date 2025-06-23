93efa4acaaaed8ce26d5a3d5ca5005726aab2719
Nikitha <mailnikitha1710@gmail.com>
2025-06-23 18:04:01 +0530

    Add PL/SQL Ex 1 Scenario 2

---
+++ WEEK_2/PL_SQL_programming/Exercise1_ControlStructures/Code/ex1_scenario2.sql
@@ -0,0 +1,21 @@
+ALTER TABLE Customers ADD IsVIP VARCHAR2(5);
+
+BEGIN
+    FOR rec IN (SELECT CustomerID, Balance FROM Customers) 
+    LOOP
+        IF rec.Balance > 10000 THEN
+            UPDATE Customers 
+            SET IsVIP = 'TRUE' 
+            WHERE CustomerID = rec.CustomerID;
+        ELSE
+            UPDATE Customers 
+            SET IsVIP = 'FALSE' 
+            WHERE CustomerID = rec.CustomerID;
+        END IF;
+    END LOOP;
+END;
+/
+
+SELECT CustomerID, Name, Balance, IsVIP
+FROM Customers
+WHERE IsVIP = 'TRUE';
