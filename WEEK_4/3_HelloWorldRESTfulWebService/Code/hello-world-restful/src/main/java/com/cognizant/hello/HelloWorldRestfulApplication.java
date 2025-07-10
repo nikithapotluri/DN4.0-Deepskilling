6991e7288120b5e69e0eefa91718c1f4210db31c
Nikitha <mailnikitha1710@gmail.com>
2025-07-10 16:55:46 +0530

    Add Spring Rest 3

---
+++ WEEK_4/3_HelloWorldRESTfulWebService/Code/hello-world-restful/src/main/java/com/cognizant/hello/HelloWorldRestfulApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRestfulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldRestfulApplication.class, args);
+    }
+}
